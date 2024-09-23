32c82d3dcb12233cb0f19d27af12460ebddbc5bb
gitBilal-Siddique-N-B-Kkc <bilalsiddique817@gmail.com>
2024-09-23 20:47:42 +0500

    para added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
     <div>
 
       <h1 className='text-center'> App is Running</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim harum, ullam facere corrupti dolor esse dignissimos nemo, temporibus doloremque et modi dolores cumque dolorem nisi similique, ex eum veritatis? Sint.</p>
 
     </div>
   )
